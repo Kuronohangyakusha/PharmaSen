9d3640d9e0c793f708fb6dbeb4cd8c300d641cb8
Your Name <your.email@example.com>
2026-03-10 15:28:13 +0000

    chore: nettoyer dist et node_modules du repo

--- dist/config/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Instance unique du client Prisma pour toute l'application
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient({
-    log: ['error', 'warn'],
-});
-exports.default = prisma;
-//# sourceMappingURL=prisma.js.map
